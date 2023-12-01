75046d9a12323b1ed0fc10e52697e72db8ed30cd
Yllen Fernandez <59622068+yllenfer@users.noreply.github.com>
2023-12-01 22:28:58 +0100

    Update dependencies and build configuration

--- dist/assets/main-D_V_PHmu.js
+++ dist/assets/main-D_V_PHmu.js
@@ -6,12 +6,8 @@
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${s}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <h1>Hello From Yllen!</h1>
+   
+   
   </div>
 `;l(document.querySelector("#counter"));
