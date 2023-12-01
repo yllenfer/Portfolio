d68a55a23945e5156eee1e7bf30d613939829a95
Yllen Fernandez <59622068+yllenfer@users.noreply.github.com>
2023-12-01 23:09:56 +0100

    Update dependencies and remove unused files

---
+++ dist/assets/main-b67b558d.js
@@ -0,0 +1,13 @@
+(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&s(c)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const i="/assets/javascript-8dac5379.svg";function l(n){let r=0;const o=s=>{r=s,n.innerHTML=`count is ${r}`};n.addEventListener("click",()=>o(r+1)),o(0)}document.querySelector("#app").innerHTML=`
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+    
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${i}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello From Yllen!</h1>
+   
+   
+  </div>
+`;l(document.querySelector("#counter"));
