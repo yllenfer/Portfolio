5c132ec0fc6f2488fecbdcd39c1c682bc9738895
Yllen Fernandez <yllenfernandez@gmail.com>
2024-02-02 16:56:01 +0100

    Refactor email sending function

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,17 +1,9 @@
 require('dotenv').config();
-const express = require('express');
 const nodemailer = require('nodemailer');
-const cors = require('cors');
 
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-
-app.post('/send-email', async (req, res) => {
+exports.handler = async (event, context) => {
     try {
-        const { firstname, email, subject } = req.body;
+        const { firstname, email, subject } = JSON.parse(event.body);
 
         let transporter = nodemailer.createTransport({
             service: 'gmail',
@@ -28,19 +20,18 @@ app.post('/send-email', async (req, res) => {
             text: `You have a new submission from:\nName: ${firstname}\nEmail: ${email}\nMessage: ${subject}`
         };
 
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.error(error);
-                return res.status(500).send("Error while sending email.");
-            }
+        const info = await transporter.sendMail(mailOptions);
+        console.log('Message sent: %s', info.messageId);
 
-            console.log('Message sent: %s', info.messageId);
-            res.status(200).send('Email sent successfully!');
-        });
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Email sent successfully!' }),
+        };
     } catch (error) {
         console.error('Error:', error);
-        res.status(500).send('Internal Server Error');
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Internal Server Error' }),
+        };
     }
-});
-
-module.exports = app;
+};
